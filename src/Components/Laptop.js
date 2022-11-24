76b24446e8f02cb18edd3d56c34559acbccd2974
Asif Ferdous <asifferdous23@gmail.com>
2022-11-24 12:23:22 +0600

    bookings

--- src/Components/Laptop.js
+++ src/Components/Laptop.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
+import { AuthContext } from './AuthProvider';
+
 
 const Laptop = ({ laptop }) => {
+    const { user, logOut } = useContext(AuthContext);
     const { product_name, seller_name, pic, years, or_price, re_price, location, code } = laptop;
+    const handleSubmit = event => {
+        
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const displayName = form.displayName.value;
+        const product_name = form.product_name.value;
+        const re_price = form.re_price.value;
+        const location = form.location.value;
+        const phone = form.phone.value;
+
+
+        const info = {
+            displayName,
+            email,
+            product_name,
+            price: re_price,
+            location,
+            phone,
+            code,
+            pic
+        }
+        fetch(`http://localhost:5000/catagory`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(info)
+    })
+        .then(res => res.json())
+        .then(newData => {
+            if(newData.acknowledged)
+            alert('Booking confirmed');
+        })
+        .catch(er => console.error(er));
+
+    }
+
+
     return (
         <div className="col">
 
@@ -15,13 +58,87 @@ const Laptop = ({ laptop }) => {
                     <div>
                         <span className="card-title mb-2 ">Seller Name: {seller_name}</span>
                         <i className="fa-solid fa-check ms-1 bg-primary rounded-5"></i>
-                        <span className="btn btn-warning ms-3">Book Now</span>
+                        <span data-bs-toggle="modal" data-bs-target="#exampleModal" className="btn btn-warning ms-3">Book Now</span>
                     </div>
                     <h5>Purchase Year: {years}</h5>
                     <h5>Location: {location}</h5>
-
                 </div>
             </div>
+
+            {user?.uid ?
+                <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="ModalLabel">{product_name}</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div id="details" className="modal-body">
+                                <form onSubmit={() => handleSubmit} className="mb-md-2 mt-md-2 ">
+
+                                    <h2 className="fw-bold mb-2 text-uppercase">Complete the form</h2>
+
+
+                                    <div className="form-outline form-white mb-4">
+                                        <input disabled defaultValue={user.displayName} type="text" name="displayName" className="form-control form-control-lg" />
+                                    </div>
+
+                                    <div className="form-outline form-white mb-4">
+                                        <input disabled defaultValue={user.email} type="email" name="email" className="form-control form-control-lg" />
+                                    </div>
+
+                                    <div className="form-outline form-white mb-4">
+                                        <input disabled defaultValue={product_name} type="text" name="product_name" className="form-control form-control-lg" />
+                                    </div>
+
+                                    <div className="form-outline form-white mb-4">
+                                        <input disabled defaultValue={re_price} type="text" name="re_price" className="form-control form-control-lg" />
+                                    </div>
+
+
+
+
+
+                                    <div className="form-outline form-white mb-4">
+                                        <input placeholder='Location' type="text" name="location" className="form-control form-control-lg" />
+                                    </div>
+
+                                    <div className="form-outline form-white mb-4">
+                                        <input placeholder='Phone Number' type="text" name="phone" className="form-control form-control-lg" />
+                                    </div>
+                                    <button className="btn btn-info px-4 " data-bs-dismiss="modal" type="submit">Submit</button>
+                                </form>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                :
+                <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="ModalLabel">{product_name}</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div id="details" className="modal-body">
+                                <h4 className="card-title mb-2">Original Price: ${or_price}</h4>
+                                <h4 className="card-title mb-2">Re-Sell Price: ${re_price}</h4>
+                                <h4 className="card-title mb-2">seller_name: {seller_name}</h4>
+                                <h2 className='text-secondary mt-4'>U need to <Link to="/login" className='text-danger'>Log In</Link> to Book</h2>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>}
         </div>
     );
 };
